208cc9d2e00cf9cd920d7f740a7e5f3e672ef182
Saneesh Saji <65639829+ssaji1@users.noreply.github.com>
2021-03-30 12:22:35 -0400

    test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+const express = require("express")
+
+const app = express()
+
+
+app.get('/', (request, response) => {
+    console.log('Request Received') 
+    // response.send('Welcome');
+    response.json({msg: 'Welcome'});
+  })
+
+app.listen(3333, () => {
+    console.log("server is listening");
+})
